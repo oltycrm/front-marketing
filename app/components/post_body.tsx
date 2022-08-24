aa76b3a387d1d605646d3106918b6913644c7600
Alex Vyber <alexvyber@gmail.com>
2022-08-25 02:02:54 +0300

    new shit

--- app/components/post_body.tsx
+++ app/components/post_body.tsx
@@ -2,5 +2,9 @@ import { marked } from "marked";
 
 export default function PostBody({ content }: any) {
   const html = marked.parse(content);
-  return <div className="max-w-2xl mx-auto"></div>;
+  return (
+    <div className="max-w-2xl mx-auto">
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+    </div>
+  );
 }
